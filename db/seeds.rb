8734326230b5bad1c23904f91a41b0c9171e7763
mxales11 <mxales11@smumn.edu>
2013-04-27 14:19:55 -0500

    User Stories 10 fixing students index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-courses = Course.create([{ name: 'Probability', credits: 3}, { name: 'Web Systems', credits: 3}, { name: 'Linear Algebra', credits: 3}, { name: 'Bible and Belief', credits: 3}, { name: 'Modern Dance', credits: 1}, { name: 'Ceramics', credits: 3}, { name: 'Calculus III', credits: 4}, { name: 'Intro to Literature', credits: 3}, { name: 'Modern Movies', credits: 3}, { name: 'Into to Geography', credits: 3}])
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -3,7 +3,6 @@ namespace :db do
 	desc "Fill database with sample data"
 	task populate: :environment do
 		admin = Student.create!(email: "mxales@smumn.edu", password: "krokus18", password_confirmation: "krokus18")
-
 		admin.toggle!(:admin)
 	end
 	
